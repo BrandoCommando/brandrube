5fa037ffacb73c1e39bee05bf1a8c8f3aab52304
Brandon Bowles <BrandonBowles@gmail.com>
2011-07-12 11:35:56 -0700

    Initial Commit

---
+++ src/com/brandroid/brandrube/RubeMain.java
@@ -0,0 +1,13 @@
+package com.brandroid.brandrube;
+IMPORT_RESOURCE_CLASS
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RubeMain extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
